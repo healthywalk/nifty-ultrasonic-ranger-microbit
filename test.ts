f6c033c26fa434f04ac64a1f5d9925d9a475da57
healthywalk <89001945+healthywalk@users.noreply.github.com>
2021-08-23 16:08:00 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
-// テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+NiftyUltrasonicRanger.initializeUltrasonicRanger(DigitalPin.P1, DigitalPin.P2)
+basic.forever(function () {
+    serial.writeLine(convertToText(NiftyUltrasonicRanger.measureincm()))
+})
